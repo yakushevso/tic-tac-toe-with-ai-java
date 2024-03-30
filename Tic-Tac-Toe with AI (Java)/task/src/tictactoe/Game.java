3a64e6bd7951f4ee32b6ea4c81357f4609b8bd88
Home <spirit40web@gmail.com>
2024-03-30 23:32:28 +0300

    Stage 3/5: Watch 'em fight

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Game.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Game.java
@@ -1,55 +1,7 @@
 package tictactoe;
 
 public class Game {
-    private final Field field;
-    private final Logic logic;
-    private final UI ui;
-
-    public Game() {
-        this.ui = new UI();
-        this.field = new Field(3);
-        this.logic = new Logic();
-    }
-
     public static void main(String[] args) {
-        new Game().start();
-    }
-
-    private void start() {
-        field.init();
-        field.print();
-        logic.update(field.getState());
-
-        Bot bot = new EasyBot(3);
-        boolean playerTurn = true;
-
-        while (logic.isNotFinished()) {
-            int[] move;
-
-            if (playerTurn) {
-                move = logic.convertToIntegers(ui.readMove());
-
-                if (move == null) {
-                    continue;
-                }
-            } else {
-                move = ui.readMoveBot(bot, field.getState());
-            }
-
-            if (logic.isValidMove(move)) {
-                if (playerTurn) {
-                    ui.makeMove(field, move, 'X');
-                } else {
-                    ui.makeMove(field, move, 'O');
-                }
-
-                logic.update(field.getState());
-                field.print();
-
-                playerTurn = !playerTurn;
-            }
-        }
-
-        logic.printResult();
+        new UI().run();
     }
 }
--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/UI.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/UI.java
@@ -1,23 +1,89 @@
 package tictactoe;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class UI {
     private final Scanner SC = new Scanner(System.in);
 
-    @SuppressWarnings("unused")
-    public int readFieldSize() {
+    public void run() {
+        Field field = new Field(3);
+        Logic logic = new Logic();
+
+        field.init();
+        logic.update(field.getState());
+
         while (true) {
-            if (SC.hasNextInt()) {
-                return SC.nextInt();
-            } else {
-                System.out.println("You should enter numbers!");
-                SC.next();
+            String[] input = readInput();
+
+            switch (input[0]) {
+                case "start" -> {
+                    field.print();
+
+                    boolean playerTurn = true;
+
+                    while (logic.isNotFinished()) {
+                        int[] move;
+
+                        if (playerTurn) {
+                            move = readMove(field, logic, input[1]);
+                        } else {
+                            move = readMove(field, logic, input[2]);
+                        }
+
+                        if (move != null && logic.isValidMove(move)) {
+                            if (playerTurn) {
+                                makeMove(field, move, 'X');
+                            } else {
+                                makeMove(field, move, 'O');
+                            }
+
+                            logic.update(field.getState());
+                            field.print();
+
+                            playerTurn = !playerTurn;
+                        }
+                    }
+
+                    logic.printResult();
+                }
+                case "exit" -> {
+                    return;
+                }
+
+                default -> System.out.println("Bad parameters!");
             }
         }
     }
 
-    public String readMove() {
+    public String[] readInput() {
+        while (true) {
+            System.out.print("Input command: ");
+            String[] input = SC.nextLine().split("\\s+");
+            Set<String> type = new HashSet<>(Arrays.asList("start", "user", "easy"));
+
+            if (input.length == 1 && "exit".equals(input[0])
+                    || input.length == 3 && type.contains(input[1]) && type.contains(input[2])) {
+                return input;
+            } else {
+                System.out.println("Bad parameters!");
+            }
+        }
+    }
+
+    private int[] readMove(Field field, Logic logic, String player) {
+        switch (player) {
+            case "user" -> {
+                return logic.convertToIntegers(readMoveUser());
+            }
+            case "easy" -> {
+                return readMoveBot(new EasyBot(3), field.getState());
+            }
+        }
+
+        return null;
+    }
+
+    public String readMoveUser() {
         System.out.print("Enter the coordinates: ");
         return SC.nextLine();
     }
@@ -33,5 +99,17 @@ public class UI {
         System.out.println("Making move level \"easy\"");
 
         return bot.makeMove(field);
+    }
+
+    @SuppressWarnings("unused")
+    public int readFieldSize() {
+        while (true) {
+            if (SC.hasNextInt()) {
+                return SC.nextInt();
+            } else {
+                System.out.println("You should enter numbers!");
+                SC.next();
+            }
+        }
     }
 }
